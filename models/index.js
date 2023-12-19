8e56761c3e120513adef520867e8b54b68a50255
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2023-12-19 15:02:26 -0600

    completed tag routes

--- models/index.js
+++ models/index.js
@@ -15,14 +15,14 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: "ProductTag",
+  through: "product_tag",
   as: "tags",
   foreignKey: "product_id",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: "ProductTag",
-  as: "tags",
+  through: "product_tag",
+  as: "products",
   foreignKey: "tag_id",
 });
 module.exports = {
